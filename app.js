796cf7c05f8d9e3e0e100cbfafadb4db747e5fc7
Tri Pham <trimpham98@gmail.com>
2022-10-20 13:56:18 -0700

    Set audio.currentTime to 0 to rewind audio from start instead of waiting

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ window.addEventListener("keydown", function (e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   if (!audio) return; // if no audio found, stops function from running altogether
 
+  audio.currentTime = 0; // rewinds audio from start instead of waiting for .wav file to finish before playing again
   audio.play(); // plays the .wav file that corresponds to the key code
 });
