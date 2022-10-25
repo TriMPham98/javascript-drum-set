b477c55aad1bf99398f51b77235f6cd11ea560da
Tri Pham <trimpham98@gmail.com>
2022-10-24 19:45:39 -0700

    Refactor constant variables to uppercase

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 function playSound(e) {
   // looks for one element with the audio tag that corresponds with the key pressed
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  if (!audio) return; // if no audio found, stops function from running altogether
+  const AUDIO = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (!AUDIO) return; // if no audio found, stops function from running altogether
 
   // looks for one element with the key tag that corresponds with the key pressed
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  const KEY = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
   // TODO: Add Hi Hat Sizzle using conditional
 
-  audio.currentTime = 0; // rewinds audio from start instead of waiting for .wav file to finish before playing again
-  audio.play(); // plays the .wav file that corresponds to the key code
+  AUDIO.currentTime = 0; // rewinds audio from start instead of waiting for .wav file to finish before playing again
+  AUDIO.play(); // plays the .wav file that corresponds to the key code
 
-  key.classList.add("playing"); // adds the css class "playing" on key press
+  KEY.classList.add("playing"); // adds the css class "playing" on key press
 }
 
 function removeTransition(e) {
   this.classList.remove("playing"); // removes the css class "playing" once transition is done
 }
 
-const keys = document.querySelectorAll(".key"); // gives an array of every element matched
-keys.forEach((key) => key.addEventListener("transitionend", removeTransition)); // calls the removeTransition function
+const KEYS = document.querySelectorAll(".key"); // gives an array of every element matched
+KEYS.forEach((key) => key.addEventListener("transitionend", removeTransition)); // calls the removeTransition function
 window.addEventListener("keydown", playSound);
